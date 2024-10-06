7937342885f0627c97b7dcb35fb8e8dea7949a9c
kuntalkumar <kuntalkumar789@gmail.com>
2024-10-06 10:59:48 +0530

    added

--- client/src/pages/Clubhouse.jsx
+++ client/src/pages/Clubhouse.jsx
@@ -19,7 +19,6 @@ const Clubhouse = () => {
       <Image
         // src="https://cdn.prod.website-files.com/619f269ed5aa1e062de9e2ce/667295810e4937572f4648c0_updated%20plan-p-2000.jpeg"  // intentionally remove pic of club hpuse
         src=""
-
         alt="Clubhouse Masterplan image asset not available "
         width="100%"
         height={{ base: "50vh", md: "80vh", lg: "95vh" }} // Responsive height
